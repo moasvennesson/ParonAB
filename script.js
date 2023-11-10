f388a3ef4dc2b4509149d4577126737f83f668c0
moasvennesson <62596297+moasvennesson@users.noreply.github.com>
2023-11-10 16:07:54 +0100

    js

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 const dropdownElementList = document.querySelectorAll('.dropdown-toggle')
 const dropdownList = [...dropdownElementList].map(dropdownToggleEl => new bootstrap.Dropdown(dropdownToggleEl))
+
+
+var myDropdown = document.getElementById('myDropdown')
+myDropdown.addEventListener('show.bs.dropdown', function () {
+  // do something...
+})
